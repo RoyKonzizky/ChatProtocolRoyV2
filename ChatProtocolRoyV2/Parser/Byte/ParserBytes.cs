3270f753c032a9187456c175c0d3299ac3549011
Roy Konzizky <roeicop110@gmail.com>
2023-05-10 01:05:10 +0300

    -finished the ExtractData method -added utility methods to ParseBytesExtensions.cs

--- ChatProtocolRoyV2/Parser/Byte/ParserBytes.cs
+++ ChatProtocolRoyV2/Parser/Byte/ParserBytes.cs
@@ -12,14 +12,15 @@ public class ParseBytes : IParseBytes
         MessageBase message = null!;
         Generate generator = new Generate();
         ChecksumByteArrayCalculator calculator = new ChecksumByteArrayCalculator();
+        ParserBytesExtensions parserExtender = new ParserBytesExtensions();
         
-        byte[] syncAndTailArray = ExtractSyncAndTail(packetBytes);
+        byte[] syncAndTailArray = ParserBytesExtensions.ExtractSyncAndTail(packetBytes);
         byte sync = syncAndTailArray[0];
         byte tail = syncAndTailArray[1];
         
-        uint checksumInPacket = ExtractChecksum(packetBytes);
+        uint checksumInPacket = parserExtender.ExtractChecksum(packetBytes);
         
-        MessageBase dataOfPacket = ExtractData(packetBytes);
+        MessageBase dataOfPacket = parserExtender.ExtractData(packetBytes);
         IEnumerable<byte> byteDataOfPacket = Generate.ObjectToByteArray(dataOfPacket); 
         uint checksumFromData = calculator.CalculateChecksum(byteDataOfPacket);
         if (checksumFromData != checksumInPacket)
@@ -27,50 +28,9 @@ public class ParseBytes : IParseBytes
             throw new ArgumentException("Unable to parse since data or checksum were corrupted.");
         }
         
-        
-
-        return message;
-    }
-    
-    
-    private static byte[] ExtractSyncAndTail(byte[] packetBytes)
-    {
-        if (packetBytes[0] == (byte)MessageEdge.Sync && packetBytes[^1] == (byte)MessageEdge.Tail)
-        {
-            byte[] arr = {};
-            arr[0] = packetBytes[0];
-            arr[1] = packetBytes[^1];
-            return arr;
-        }
-        throw new ArgumentException("Unable to parse since Sync or Tail were corrupted.");
-    }
-
-    private static uint ExtractChecksum(byte[] packetBytes)
-    {
-        byte[] reversedPacketBytes = new byte[packetBytes.Length];
-        Array.Copy(packetBytes, reversedPacketBytes, packetBytes.Length);
-        Array.Reverse(reversedPacketBytes);
-
-        byte[] reversedChecksum = new byte[4];
-        Array.Copy(reversedPacketBytes, 1, reversedChecksum, 0, 4);
-        Array.Reverse(reversedChecksum);
-
-        Generate generator = new Generate();
-        
-        uint checksum = Generate.FromByteArray<uint>(reversedChecksum);
-        return checksum;
-    }
-    
-    
-    private static MessageBase ExtractData(byte[] packetBytes)
-    {
-        MessageBase message;
-        
         return message;
     }
     
 }
-//TODO look for a way to do this without the reverse
-//TODO finish the ExtractData method
 //since the data already contains the guid and the type its pointless for them to be in the packet
 //parser is the reverse process of the generator
---
+++ ChatProtocolRoyV2/Parser/Byte/ParserBytesExtensions.cs
@@ -0,0 +1,44 @@
+﻿using ChatProtocolRoyV2.Data;
+using ChatProtocolRoyV2.Entities;
+using ChatProtocolRoyV2.Generator;
+
+namespace ChatProtocolRoyV2.Parser.Byte;
+
+public class ParserBytesExtensions
+{
+    public static byte[] ExtractSyncAndTail(byte[] packetBytes)
+    {
+        if (packetBytes[0] != (byte)MessageEdge.Sync || packetBytes[^1] != (byte)MessageEdge.Tail)
+            throw new ArgumentException("Unable to parse since Sync or Tail were corrupted.");
+        byte[] arr = {};
+        arr[0] = packetBytes[0];
+        arr[1] = packetBytes[^1];
+        return arr;
+    }
+
+    public uint ExtractChecksum(byte[] packetBytes)
+    {
+        var reversedPacketBytes = new byte[packetBytes.Length];
+        Array.Copy(packetBytes, reversedPacketBytes, packetBytes.Length);
+        Array.Reverse(reversedPacketBytes);
+
+        var reversedChecksum = new byte[4];
+        Array.Copy(reversedPacketBytes, 1, reversedChecksum, 0, 4);
+        Array.Reverse(reversedChecksum);
+
+        Generate generator = new Generate();
+        
+        var checksum = Generate.FromByteArray<uint>(reversedChecksum);
+        return checksum;
+    }
+
+
+    public MessageBase ExtractData(byte[] packetBytes)
+    {
+        var arrMessageBase = new MessageBase[] { };
+        Array.Copy(packetBytes, 1, arrMessageBase, 0, packetBytes.Length-5);
+        var message = arrMessageBase[0];
+        return message;
+    }
+}
+//TODO look for a way to do this without the reverse
