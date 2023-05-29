999b600f8ada40be82c58a5f2cf5739e1b60cb23
Roy Konzizky <roeicop110@gmail.com>
2023-05-29 10:31:43 +0300

    -checksum calculation now uses a foreach to go over the bytes and sum them.

--- ChatProtocolRoyV2/ChecksumCalculator/Byte/ChecksumByteArrayCalculator.cs
+++ ChatProtocolRoyV2/ChecksumCalculator/Byte/ChecksumByteArrayCalculator.cs
@@ -4,22 +4,12 @@ public class ChecksumByteArrayCalculator : IChecksumByteArrayCalculator
 {
     public uint CalculateChecksum(IEnumerable<byte> input)
     {
-        //todo replace with foreach sum all the bytes values to one signature value(uint)
-        var enumerable = input as byte[] ?? input.ToArray();
-        var length = enumerable.Length;
-        var i = 0;
-
         uint sum = 0;
-        while (length > 1)
+        foreach (byte b in input)
         {
-            sum += (ushort)((enumerable[i++] << 8) | enumerable[i++]);
-            length -= 2;
+            sum += b;
         }
 
-        if (length > 0) sum += (ushort)(enumerable[i] << 8);
-
-        while (sum >> 16 != 0) sum = (sum & 0xFFFF) + (sum >> 16);
-
-        return ~sum;
+        return sum;
     }
 }
