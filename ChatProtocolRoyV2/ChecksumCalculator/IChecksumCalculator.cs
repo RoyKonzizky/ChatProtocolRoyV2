eb92235f8771ecdf33c4918cff628a719b53f920
Roy Konzizky <roeicop110@gmail.com>
2023-05-29 10:31:43 +0300

    -checksum calculation now uses a foreach to go over the bytes and sum them.

--- ChatProtocolRoyV2/ChecksumCalculator/IChecksumCalculator.cs
+++ ChatProtocolRoyV2/ChecksumCalculator/IChecksumCalculator.cs
@@ -4,4 +4,3 @@ public interface IChecksumCalculator<in TInput>
 {
     uint CalculateChecksum(TInput input);
 }
-//TODO add startindex and endindex-can be added and used need to ask how necessary as the parser already can work without it, however it will fill the empty spot of the type
