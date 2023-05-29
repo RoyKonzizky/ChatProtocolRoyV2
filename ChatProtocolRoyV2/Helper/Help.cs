53c4504c50d628281e609562e9237e9269786e5e
Roy Konzizky <roeicop110@gmail.com>
2023-05-29 16:00:23 +0300

    -ObjectToByteArray and FromByteArray now check if the object type is one of the types used in the code

--- ChatProtocolRoyV2/Helper/Help.cs
+++ ChatProtocolRoyV2/Helper/Help.cs
@@ -1,3 +1,5 @@
+using ChatProtocolRoyV2.Entities;
+
 namespace ChatProtocolRoyV2.Helper;
 
 public class Help
@@ -5,9 +7,10 @@ public class Help
     public byte[] ObjectToByteArray<T>(T obj)
     {
         //TODO change from T to a more concrete types from this project, security concerns, use design patterns
+        var concreteType = ConvertToConcreteType(obj);
         using var memoryStream = new MemoryStream();
         using var binaryWriter = new BinaryWriter(memoryStream);
-        var objString = obj!.ToString()!;
+        var objString = concreteType.ToString()!;
         binaryWriter.Write(objString);
         return memoryStream.ToArray();
     }
@@ -16,8 +19,63 @@ public class Help
     {
         using var memoryStream = new MemoryStream(byteArray);
         using var binaryReader = new BinaryReader(memoryStream);
-        var objString = binaryReader.ReadString();
-        return (T)(object)objString;
+
+        object objValue;
+
+        if (typeof(T) == typeof(MessageType))
+        {
+            var messageType = (MessageType)binaryReader.ReadInt32();
+            objValue = messageType;
+        }
+        else if (typeof(T) == typeof(Guid))
+        {
+            var guidBytes = binaryReader.ReadBytes(16);
+            objValue = new Guid(guidBytes);
+        }
+        else if (typeof(T) == typeof(string))
+        {
+            objValue = binaryReader.ReadString();
+        }
+        else if (typeof(T) == typeof(int))
+        {
+            objValue = binaryReader.ReadInt32();
+        }
+        else if (typeof(T) == typeof(MessageEdge))
+        {
+            var messageEdge = (MessageEdge)binaryReader.ReadByte();
+            objValue = messageEdge;
+        }
+        else if (typeof(T) == typeof(uint))
+        {
+            objValue = binaryReader.ReadUInt32();
+        }
+        else if (typeof(T) == typeof(DateOnly))
+        {
+            var dateOnlyString = binaryReader.ReadString();
+            objValue = DateOnly.Parse(dateOnlyString);
+        }
+        else if (typeof(T) == typeof(FileTypes))
+        {
+            var fileType = (FileTypes)binaryReader.ReadInt32();
+            objValue = fileType;
+        }
+        else
+        {
+            throw new ArgumentException("Unsupported type.");
+        }
+
+        return (T)ConvertToConcreteType(objValue);
+    }
+
+
+    private object ConvertToConcreteType<T>(T obj)
+    {
+        if (obj is MessageType or Guid or string or int or MessageEdge or uint or DateOnly or FileTypes)
+        {
+            return obj;
+        }
+
+        throw new ArgumentException("Unsupported type.");
     }
 
     public static string FileDataToBase64(string filePath)
