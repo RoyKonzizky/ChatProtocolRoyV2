e110deb5c7cd29b4e8ed356bebbc21585390c486
Roy Konzizky <roeicop110@gmail.com>
2023-06-04 09:35:39 +0300

    -Used Encoding.ASCII to rewrite the functions of the helper

--- ChatProtocolRoyV2/Constants/Encodings.cs
+++ ChatProtocolRoyV2/Constants/Encodings.cs
@@ -4,7 +4,8 @@ namespace ChatProtocolRoyV2.Constants;
 
 public static class Encodings
 {
-    static Encoding _ascii = Encoding.ASCII;
-
+    public static readonly Encoding ASCII = Encoding.ASCII;
 }
+
+
 //TODO make this a constant class instead of the monstrosity in the helper module
